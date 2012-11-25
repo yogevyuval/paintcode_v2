d6929d5c0a4bb98fc01bb3e60c8481f3a15542f2
Michael Lantsman <michlan@walla.com>
2012-11-25 21:55:26 +0200

    Motion-adding classes

---
+++ Entities/Motion.cpp
@@ -0,0 +1,9 @@
+//
+//  Motion.cpp
+//  PaintCode_V2
+//
+//  Created by Michael Lantsman on 11/25/12.
+//  Copyright (c) 2012 Michael Lantsman. All rights reserved.
+//
+
+#include "Motion.h"
---
+++ Entities/Motion.h
@@ -0,0 +1,14 @@
+//
+//  Motion.h
+//  PaintCode_V2
+//
+//  Created by Michael Lantsman on 11/25/12.
+//  Copyright (c) 2012 Michael Lantsman. All rights reserved.
+//
+
+#ifndef __PaintCode_V2__Motion__
+#define __PaintCode_V2__Motion__
+
+#include <iostream>
+
+#endif /* defined(__PaintCode_V2__Motion__) */
