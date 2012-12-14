94402304391b166b463a49d36276c4b1b6f355e5
Yuval Yogev <yogevyuval@gmail.com>
2012-12-14 11:52:15 +0200

    Fixing conflict 2

--- Entities/MotionHandler.cpp
+++ Entities/MotionHandler.cpp
@@ -20,7 +20,6 @@
 //        return;
 //}
 //
-<<<<<<< HEAD
 //bool MotionHandler::checkForStart(Candidate c)
 //{
 //    float x = c.getX(), y = c.getY(), r = c.getRadius();
@@ -29,29 +28,4 @@
 //    }
 //    return false;
 //}
-=======
 //
-
-#include "MotionHandler.h"
-
-void MotionHandler::feed(Candidate cand)
-{
-//    if(MotionHandler::currentState == MotionHandler::state.WAITING_FOR_START)
-//    {
-//        if(checkForStart(cand))
-//            MotionHandler::currentState = MotionHandler::state.RECORDING;
-//    }
-//    else if(MotionHandler::currentState == MotionHandler::state.RECORDING){}
-//    else if(MotionHandler::currentState == MotionHandler::state.FINISHED){}
-//        return;
-}
-
-bool MotionHandler::checkForStart(Candidate c)
-{
-    float x = c.getX(), y = c.getY(), r = c.getRadius();
-    if(x > 20 && x < 70 && y > 20 && y < 70){
-        return true;
-    }
-    return false;
-}
->>>>>>> Fixes and Improvments
