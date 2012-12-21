c22669f9d9e0118cf26173c772bcca98eb9343b4
Yuval Yogev <yogevyuval@gmail.com>
2012-12-21 12:44:28 +0200

    Shira's Motion Handler, Deleted Frame.c

--- Entities/MotionHandler.cpp
+++ Entities/MotionHandler.cpp
@@ -1,31 +1,67 @@
-////
-////  MotionHandler.cpp
-////  CvTest
-////
-////  Created by Yuval Yogev on 12/1/12.
-////
-////
 //
-//#include "MotionHandler.h"
+//  MotionHandler.cpp
+//  CvTest
 //
-//void MotionHandler::feed(Candidate cand)
-//{
-//    if(MotionHandler::currentState == MotionHandler::state.WAITING_FOR_START)
-//    {
-//        if(checkForStart(cand))
-//            MotionHandler::currentState = MotionHandler::state.RECORDING;
-//    }
-//    else if(MotionHandler::currentState == MotionHandler::state.RECORDING){}
-//    else if(MotionHandler::currentState == MotionHandler::state.FINISHED){}
-//        return;
-//}
+//  Created by Yuval Yogev on 12/1/12.
 //
-//bool MotionHandler::checkForStart(Candidate c)
-//{
-//    float x = c.getX(), y = c.getY(), r = c.getRadius();
-//    if(x > 20 && x < 70 && y > 20 && y < 70){
-//        return true;
-//    }
-//    return false;
-//}
 //
+
+#include "MotionHandler.h"
+
+MotionHandler::MotionHandler()
+{
+    currentState = MotionHandler::WAITING_FOR_START;
+    toSearch = cvRect(20, 20, 50, 50);
+    MotionHandler::m = Motion();
+}
+
+int MotionHandler::getState()
+{
+    return currentState;
+}
+
+void MotionHandler::feed(Candidate* cand)
+{
+    if(currentState == MotionHandler::WAITING_FOR_START)
+    {
+        if(checkForStart(cand))
+            currentState = MotionHandler::RECORDING;
+    }
+    else if(currentState == MotionHandler::RECORDING){
+        addToDeque(cand);
+        //just for testing...
+        if(!checkForStart(cand))
+            currentState = MotionHandler::WAITING_FOR_START;
+    }
+    else if(currentState == MotionHandler::FINISHED){}
+        return;
+}
+
+bool MotionHandler::checkForStart(Candidate* c)
+{
+    float x = c->getX(), y = c->getY(), r = c->getRadius();
+    if(x > toSearch.x && x < toSearch.x + toSearch.width && y > toSearch.y && y < toSearch.y + toSearch.height){
+        cout << "!!!" << endl;
+        return true;
+    }
+    return false;
+}
+
+Motion MotionHandler::getMotion()
+{
+    return m;
+}
+
+CvRect MotionHandler::getSearchArea()
+{
+    return MotionHandler::toSearch;
+}
+
+void MotionHandler::addToDeque(Candidate * c)
+{
+    recentRecords.push_front(c);
+        if(recentRecords.size() > maxRecordSaves)
+        {
+            recentRecords.pop_front();
+        }
+}
--- Entities/MotionHandler.h
+++ Entities/MotionHandler.h
@@ -1,52 +1,61 @@
-////
-////  MotionHandler.h
-////  CvTest
-////
-////  Created by Yuval Yogev on 12/1/12.
-////
-////
 //
-//#ifndef __CvTest__MotionHandler__
-//#define __CvTest__MotionHandler__
+//  MotionHandler.h
+//  CvTest
 //
-//#include <iostream>
-//#include "Motion.h"
-//#include "Candidate.h"
-//#include <cvaux.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-//#include <cxcore.hpp>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <assert.h>
-//#include <math.h>
-//#include <float.h>
-//#include <limits.h>
-//#include <time.h>
-//#include <ctype.h>
-//#include <math.h>
-//#include <iostream>
+//  Created by Yuval Yogev on 12/1/12.
 //
-//#endif /* defined(__CvTest__MotionHandler__) */
 //
-//class MotionHandler {
-//public:
-//    typedef enum{
-//        WAITING_FOR_START, //The handler is waiting for the user to start the motion
-//        RECORDING,         //We got the motion and we are now recording it
-//        FINISHED           //The user finished the motion and we can now provide the motion.
-//    }state;
-//    //Motion getMotion(); I need the motion class for this.
-//    void record();
-//    void feed(Candidate cand);
-//    state getState();
-//
-//private:
-//    state currentState;
-//    Rect toSearch = Rect(20,20,50 ,50);
-//    void updateState(); //This method will check the right things according to the current state.
-//    bool checkForStart(Candidate c);
-//    bool checkForEnd();
-//
-//};
 
+#ifndef __CvTest__MotionHandler__
+#define __CvTest__MotionHandler__
+
+#include <iostream>
+#include "Motion.h"
+#include "Candidate.h"
+#include <cvaux.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cxcore.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#include <ctype.h>
+#include <math.h>
+#include <iostream>
+#include <deque>
+
+
+using namespace cv;
+using namespace std;
+
+class MotionHandler {
+public:
+    static const int WAITING_FOR_START = 0, //The handler is waiting for the user to start the motion
+        RECORDING = 1,         //We got the motion and we are now recording it
+        FINISHED = -1;           //The user finished the motion and we can now provide the motion.
+    static const int maxRecordSaves = 20; //maximum size of the deque
+    Motion getMotion();
+    void record();
+    void feed(Candidate* cand);
+    int getState();
+    CvRect getSearchArea();
+    MotionHandler();
+
+private:
+
+    deque<Candidate *> recentRecords;
+    Motion m;
+    CvRect toSearch;
+    int currentState;
+    void updateState(); //This method will check the right things according to the current state.
+    bool checkForStart(Candidate* c);
+    bool checkForEnd();
+    void addToDeque(Candidate* c);
+
+};
+
+#endif /* defined(__CvTest__MotionHandler__) */
